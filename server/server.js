7eaf279dfbaba3f3177a371b36bdad0a1ea4ccab
Piyush Ranjan <piyush@Piyushs-MacBook-Air.local>
2026-02-25 12:40:24 +0530

    Fixed port for deployment

--- server/server.js
+++ server/server.js
@@ -131,6 +131,8 @@ Keep answers short.
 
 
 // start server
-app.listen(3000, ()=>{
-  console.log("🚀 Server running → http://127.0.0.1:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+  console.log("Server running...");
 });
